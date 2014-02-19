427f3f7c10fc0a7002d3a5cd9d0cf713e1ca1447
Rene Hopf <renehopf@mac.com>
2014-02-19 22:27:56 +0100

    aufräumen

--- contour.hpp
+++ contour.hpp
@@ -17,18 +17,7 @@
 
 #include "knuthcam.hpp"
 
-//void findcontours(drawing_t &d);
-//void showopen(drawing_t &d);
-//void showclosed(drawing_t &d);
-//void showsegments(drawing_t &d);
 void displaycontour(contur* c);
 void arrow(glm::vec2,glm::vec2);
-//bool intersec(seg_t s1, seg_t s2, float &p, float &t);
-//void offset(layer_t &c,float);
-//void join(seg_t &s1, seg_t &s2,cont_t &c,glm::vec2,float);
-//float angle(glm::vec2,glm::vec2);
-//float angle(glm::vec2 a, glm::vec2 b);
-//float angle(glm::vec2 a);
-//bool turn(cont_t);
 
 #endif
